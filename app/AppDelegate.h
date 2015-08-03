01363fdd5418113080e4b7181ea08974ccefc8f6
yangxiaobing0725 <2797422923@qq.com>
2015-08-03 10:48:51 +0800

    Initial Commit

---
+++ app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app
+//
+//  Created by kpkj-ios on 15/8/3.
+//  Copyright (c) 2015年 kpkj-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app
+//
+//  Created by kpkj-ios on 15/8/3.
+//  Copyright (c) 2015年 kpkj-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
