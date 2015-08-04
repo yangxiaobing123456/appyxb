f8b0ec7f29fc129bb0abe9d97a44c9704373b11a
yangxiaobing0725 <2797422923@qq.com>
2015-08-04 12:10:13 +0800

    设置权限为杨小兵

--- app/UIBarButtonItem+YXBCreate.h
+++ app/UIBarButtonItem+YXBCreate.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (YXBCreate)
+/**
+ *  <#Description#>
+ *
+ *  @param imageName     <#imageName description#>
+ *  @param highImageName <#highImageName description#>
+ *  @param target        <#target description#>
+ *  @param action        <#action description#>
+ *
+ *  @return <#return value description#>
+ */
 + (instancetype)barButtonItemWithImage:(NSString *)imageName highImage:(NSString *)highImageName target:(id)target action:(SEL)action;
 @end
